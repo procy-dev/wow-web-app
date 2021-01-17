dafddda62e13b19aa367f0cc8efdc1e76a82be41
procyon4 <rchrisw4@gmail.com>
2021-01-16 18:28:00 -0800

    pushing before showing to mike

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+export const AuthContext = React.createContext(); 
+
+const initialState = {
+    isAuth: false,
+    user: null,
+    token: null,
+};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN":
+            localStorage.setItem("user", JSON.stringify(action.payload.user));
+            return {
+                ...state,
+                isAuth: true,
+                user: action.payload.user,
+                token: action.payload.token
+            };
+        case "LOGOUT":
+            localStorage.clear();
+            return {
+                ...state,
+                isAuth: false,
+                user: null
+            };
+        default:
+            return state;
+    }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    return (
+        <AuthContext.Provider
+            value={{
+                state,
+                dispatch
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Header />
+            <div className="App">{!state.isAuth ? <Login /> : <Home />}</div>
+        </AuthContext.Provider>
+    );
 }
-
 export default App;
